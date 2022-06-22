27c62f0301ffb497b515a2dea018e8725764ac8c
Daniil <vetusst@hotmail.com>
2022-06-22 21:50:15 +0200

    add mui themes

---
+++ src/app/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles"; 
+
+export const theme = createTheme({  
+    palette: {    
+        primary: {      
+            main: '#8a2b06',
+            light: '#ffffff',      
+            dark: '#383838',      
+        },    
+    }
+})
