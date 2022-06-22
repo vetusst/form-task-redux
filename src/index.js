3351426fba641fca9d45f7b32eff5d7d01b3b7e5
Daniil <vetusst@hotmail.com>
2022-06-22 21:51:36 +0200

    add localization and theme providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import "./index.css";
 
-const container = document.getElementById('root');
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from '../src/app/theme.js'
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </LocalizationProvider>
     </Provider>
-  </React.StrictMode>
 );
